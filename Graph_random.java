d2e370bb02a8467ab6a2735afc89c43bfdd7d61a
Tom Albrecht <74017910+z0mpY@users.noreply.github.com>
2024-07-09 19:38:23 +0200

    Update Graph_random.java

--- Graph_random.java
+++ Graph_random.java
@@ -90,7 +90,7 @@ class Graph {
                     break;
                 }
             }
-            totalWeight += weight / Math.pow(1 + 0.00, i);
+            totalWeight += weight / Math.pow(1 + 0.05, i);
         }
         return totalWeight;
     }
@@ -136,47 +136,119 @@ class Graph {
         }
     }
 
-    public static void main(String[] args) {
-        Graph g = new Graph(10);
+    
+    public static void main(String[] args) throws InterruptedException{
+        Graph g = new Graph(37);
 
         g.addEdge(0, 1, 200);
-        g.addEdge(1,0,800);
+        g.addEdge(0, 2, 300);
+        g.addEdge(0, 3, 500);
+        g.addEdge(0, 4, 700);
+        g.addEdge(0, 18, 1400);
+        g.addEdge(0, 17, 1500);
+        g.addEdge(0, 16, 1900);
+        g.addEdge(0, 15, 2000);
+        g.addEdge(0, 14, 2100);
+        g.addEdge(1,0, 100);
+        g.addEdge(2, 0, 100);
+        g.addEdge(3, 0, 100);
+        g.addEdge(4, 0, 100);
+        g.addEdge(18, 0, 100);
+        g.addEdge(17, 0, 100);
+        g.addEdge(16, 0, 100);
+        g.addEdge(15, 0, 100);
+        g.addEdge(14, 0, 100);
+        g.addEdge(2, 3, 500);
+        g.addEdge(3, 2,300);
         g.addEdge(1,2,300);
         g.addEdge(2,1,200);
-        g.addEdge(2,3,1000);
-        g.addEdge(3,2,300);
-        g.addEdge(3,7,700);
-        g.addEdge(3,4,300);
-        g.addEdge(7,3,1000);
-        g.addEdge(4,3,1000);
-        g.addEdge(3,5,800);
-        g.addEdge(5,3,1000);
-        g.addEdge(5,6,500);
-        g.addEdge(6,5, 800);
-        g.addEdge(7,6,500);
-        g.addEdge(6,7,700);
-        g.addEdge(7,8,600);
-        g.addEdge(8,7,700);
-        g.addEdge(6,9,300);
-        g.addEdge(9,5,500);
+        g.addEdge(1,10,2600);
+        g.addEdge(10,1,200);
+        g.addEdge(10,6,2500);
+        g.addEdge(6,10,2600);
+        g.addEdge(10,11,2700);
+        g.addEdge(11,10,2600);
+        g.addEdge(11,12,2800);
+        g.addEdge(12,11,2700);
+        g.addEdge(12,13,2900);
+        g.addEdge(13,12,2800);
+        g.addEdge(6,7,2300);
+        g.addEdge(7,6,2500);
+        g.addEdge(7,8,2200);
+        g.addEdge(8,7,2300);
+        g.addEdge(7,9,2400);
+        g.addEdge(9,7,2400);
+        g.addEdge(6,5,600);
+        g.addEdge(5,6,2500);
+        g.addEdge(5,4,700);
+        g.addEdge(4,5,600);
+        g.addEdge(4,19,1200);
+        g.addEdge(19,4,700);
+        g.addEdge(19,18,1400);
+        g.addEdge(18,19,1200);
+        g.addEdge(18,23,1600);
+        g.addEdge(23,18,1400);
+        g.addEdge(23,24,1700);
+        g.addEdge(24,23,1600);
+        g.addEdge(19,20,1100);
+        g.addEdge(20,29,1200);
+        g.addEdge(20,21,800);
+        g.addEdge(21,20,1100);
+        g.addEdge(20,22,1300);
+        g.addEdge(22,20,1100);
+        g.addEdge(22,18,1400);
+        g.addEdge(18,22,1300);
+        g.addEdge(22,26,900);
+        g.addEdge(22,25,1000);
+        g.addEdge(26,22,1300);
+        g.addEdge(25,22,1300);
+        g.addEdge(14,27,3000);
+        g.addEdge(27,14,2100);
+        g.addEdge(27,29,1800);
+        g.addEdge(29,25,3000);
+        g.addEdge(13,17,3000);
+        g.addEdge(27,13,2900);
+        g.addEdge(27,28,3100);
+        g.addEdge(28,27,3000);
+        g.addEdge(28,30,3200);
+        g.addEdge(30,28,3100);
+        g.addEdge(28,31,3300);
+        g.addEdge(31,28,3100);
+        g.addEdge(27,32,3400);
+        g.addEdge(32,27,3000);
+        g.addEdge(31,32,3400);
+        g.addEdge(32,31,3300);
+        g.addEdge(32,33,3500);
+        g.addEdge(33,32,3400);
+        g.addEdge(32,34,3600);
+        g.addEdge(34,32,3400);
+        g.addEdge(34,35,3700);
+        g.addEdge(35,34,3600);
+        g.addEdge(35,36,400);
+        g.addEdge(36,35,3700);
+        
 
         // Print the adjacency matrix before generating build orders
         g.printMatrix(g.adjacencyMatrix, "Adjacency Matrix");
         g.printReachable();
 
-        List<Integer> startNodes = Arrays.asList(0, 1);
+        List<Integer> startNodes = Arrays.asList(0, 1,9);
         Set<List<Integer>> allBuildOrders = new HashSet<>();
+        
+        
 
-        for (int i = 0; i < 1000; i++) {
+        long before =System.currentTimeMillis();
+        while (allBuildOrders.size()<1000 && System.currentTimeMillis() < before + 10000) {
             List<Integer> buildOrder = g.findOptimalPathFromMultipleSources(startNodes);
             allBuildOrders.add(buildOrder);
         }
-
+        
         System.out.println("All Build Orders:");
         int count = 1;
         for (List<Integer> buildOrder : allBuildOrders) {
             double totalWeight = g.calculateTotalWeight(buildOrder);
-            System.out.println(count + ": " + buildOrder + " (Total Weight: " + totalWeight + ")");
+            String totalWeightRounded = String.format("%.2f",totalWeight);
+            System.out.println(count + ": " + buildOrder + " (Total Weight: " + totalWeightRounded + ")");
             count++;
         }
     }
